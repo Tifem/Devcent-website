a4dd10563eed7cb1ada71065149a76aac24d58cc
Emmanuel Boluwatife Janet <88049758+Tifem@users.noreply.github.com>
2024-05-29 05:22:16 -0700

    conditional statement

--- index.js
+++ index.js
@@ -58,3 +58,88 @@ const person = {
     eyeColor: "black"
 }
 console.log(person.firstname)
+
+function addNumber(){
+    a = 5
+    b= 7
+    d = a+b
+    console.log(d)
+}
+
+addNumber()
+
+function mul(a,b){
+    a = 30;
+    b = 20;
+    c = a * b
+    console.log(c)
+}
+mul(a,b)
+
+function myFunction(p1, p2){
+    return p1 * p2;
+}
+
+var result = myFunction(5,9);
+console.log(result)
+
+function calculateArea(length, width){
+    return length + width;
+}
+
+var area = calculateArea(10,45);
+console.log(area)
+
+
+function isEven(number){
+    return number %2 === 0;
+}
+
+var check = isEven(4);
+console.log(check);
+
+
+function isOdd(number){
+    return number %2 === 1;
+}
+var check1 = isOdd(7);
+console.log(check1)
+
+function person1(){
+   const object = {
+    name: "Alice",
+    age: 70,
+    color: "black",
+    weight: 72,
+}
+console.log(object.name)
+}
+
+person1()
+
+function createPerson(name,email,age){
+    this.name= name;
+    this.email = email;
+    this.age = age;
+
+    this.greet = function(){
+        return "Hello, my name is " + this.name + "!"
+    };
+}
+
+var createPerson1 = new createPerson("Janet", "janet@gmail.com", 28)
+// console.log(createPerson1)
+console.log(createPerson1.greet())
+console.log("My name is ", createPerson1.name)
+console.log("and my email is ", createPerson1.email)
+console.log("i am ", createPerson1.age, "years old")
+
+//conditional statement
+var day = "Monday"
+
+if(day == "Monday"){
+    // alert("Today is monday")
+} else if(day == "Tuesday"){
+// alert("Today ")
+}
+
